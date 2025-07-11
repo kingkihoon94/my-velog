96fdfbafd87dfc7fec179cc86eb8caeb74a4e51f
kingkihoon94 <kh06089@naver.com>
2025-07-11 14:31:06 +0900

    [Feat] my-velog 프로젝트 설정 및 앱 라우터 확인용 about 페이지 구현

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+// src/app/about/page.tsx
+export default function AboutPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">About Page</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
